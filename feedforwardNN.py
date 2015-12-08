fixed overflow issue

feedforwardNN.py
@@ -36,10 +36,11 @@ class feedforwardNN:
         self.activ_func = sigmoid
 
         # weight matrices
-        self.W_hi = np.random.normal(loc=0, scale=1/np.sqrt(hidden_size),
+        self.W_hi = np.random.normal(loc=0, scale=.5,
                                      size=(hidden_size, input_size))
+        print self.W_hi
         self.B_h = np.random.normal(loc=0, scale=1, size=(hidden_size, 1))
-        self.W_oh = np.random.normal(loc=0, scale=1/np.sqrt(hidden_size),
+        self.W_oh = np.random.normal(loc=0, scale=.5,
                                      size=(output_size, hidden_size))
         # no bias vector on output because it's in the memory_network
 

global_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit
 
 """Global variables and utility methods.
 
@@ -21,7 +22,8 @@ def string_to_vector(s):
 
 def sigmoid(x):
     """Sigmoid activation function. Works on scalars, vectors, matrices."""
-    return 1.0 / (1.0+np.exp(-1.0*x))
+    return expit(x)
+    # return 1.0 / (1.0+np.exp(-1.0*x))
 
 
 def logit(x):

main.py
@@ -28,6 +28,7 @@ def load_data(binary, ortho):
 
 
 def main():
+    # np.seterr(invalid='raise')
     hidden_layer_size = 10
     exemplar_nodes = 200
 
@@ -44,7 +45,7 @@ def main():
     memory = alcove(input_size, output_size, exemplar_nodes, r=2)
 
     memory_net = memory_network(canonical, memory, input_size, output_size)
-    memory_net.train(ipats_binaries, tpats_binaries, 5)
+    memory_net.train(ipats_binaries, tpats_binaries, 200)
 
 if __name__ == "__main__":
     main()